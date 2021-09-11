29cfb3395779aec230f96b6211364cbae82d15af
arifulhasantotul <77568259+arifulhasantotul@users.noreply.github.com>
2021-09-11 21:22:26 +0600

    navbar style done

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById('menu_bars');
+const search = document.getElementById('search_icon');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    
+});
