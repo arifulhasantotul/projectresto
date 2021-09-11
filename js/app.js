f7805f9ec076a5eed389a2eb311e59eafb0a9f03
arifulhasantotul <77568259+arifulhasantotul@users.noreply.github.com>
2021-09-11 21:32:12 +0600

    navbar toggle done

--- js/app.js
+++ js/app.js
@@ -3,5 +3,7 @@ const search = document.getElementById('search_icon');
 const navbar = document.querySelector('.navbar');
 
 menu.addEventListener('click', () => {
-    
+    // toggle use for classlist to add or remove
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 });
