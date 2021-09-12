48e59487e7c17a65de909ad9ae7da2f243f2dac9
arifulhasantotul <77568259+arifulhasantotul@users.noreply.github.com>
2021-09-12 21:04:31 +0600

    footer done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const searchForm = document.getElementById('search_form');
 const searchBtn = document.getElementById('search_icon');
 const closeBtn = document.getElementById('close');
 const navbar = document.querySelector('.navbar');
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header .navbar a');
 
 // navbar toggle done 
 menu.addEventListener('click', () => {
@@ -14,6 +16,20 @@ menu.addEventListener('click', () => {
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+    
+    sections.forEach(section => {
+        const top = window.scrollY;
+        const height = section.offsetHeight;
+        const offset = section.offsetTop - 150;
+        const id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            });
+        }
+    });
 }
 // search modal open 
 searchBtn.onclick = () => {
